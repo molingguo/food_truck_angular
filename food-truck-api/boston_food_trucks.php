4aaca3576b8bc4f2f74980ee16fcf0e54b93ef1f
morning2139 <molingguo@Molings-MacBook-Air.local>
2015-10-16 14:20:23 -0400

    connect API and grab basic data source for display

---
+++ food-truck-api/boston_food_trucks.php
@@ -0,0 +1,152 @@
+<?php
+
+class BostonFoodTrucks {
+	
+	private $food_truck_schedule_url = 'http://www.cityofboston.gov/business/mobile/schedule-app.asp?v=1&71';
+	private $food_truck_html = '';
+	private $dom = null;
+	private $finder = null;
+	
+	private $class_company = 'com';
+	private $class_day_of_the_week = 'dow';
+	private $class_time_of_day = 'tod';
+	private $class_location = 'loc';
+	
+	private $food_trucks_nodes = array(
+		'companies' => null,
+		'company_urls' => null,
+		'days_of_week' => null,
+		'times_of_day' => null,
+		'locations' => null,
+	);
+	
+	public $food_truck_output = array(
+		'food_trucks' => array()
+	);
+	
+	public $locations_downtown = array(
+		'Boston Common, Brewer Fountain by Tremont and Boylston Streets',
+		'Carousel',
+		'Chinatown, Boylston St near Washington St',
+		'City Hall Plaza, Fisher Park',
+		'Dudley Square at Dudley street',
+		'Financial District, Milk and Kilby Streets',
+		'Financial District, Pearl Street at Franklin',
+		'Greenway, Chinatown Park',
+		'Greenway, Dewey Square Park Plaza',
+		'Greenway, Dewey Square Park at Congress St',
+		'Greenway, Echelman Sculpture at Oliver St',
+		'Greenway, Fort Point Park',
+		'Greenway, Rings Fountain at Milk St',
+		'Greenway, Rowes Wharf Plaza',
+		'Hurley Building, New Chardon and Cambridge St',
+		'Innovation District, Seaport Blvd at Boston Wharf Rd'
+	);
+	
+	
+	public function __construct () {
+		$this->food_truck_html = file_get_contents($this->food_truck_schedule_url);
+		$this->dom = new DOMDocument;
+		$this->dom->loadHTML($this->food_truck_html);
+		$this->finder = new DomXPath($this->dom);
+		
+		$this->food_truck_nodes['companies'] = 
+			$this->finder->query('//td[@class="' . $this->class_company . '"]/a/text()');
+		$this->food_truck_nodes['company_urls'] = 
+			$this->finder->query('//td[@class="' . $this->class_company . '"]/a/@href');
+		$this->food_truck_nodes['days_of_week'] = 
+			$this->finder->query('//td[@class="' . $this->class_day_of_the_week . '"]/text()');
+		$this->food_truck_nodes['times_of_day'] = 
+			$this->finder->query('//td[@class="' . $this->class_time_of_day . '"]/text()');
+		$this->food_truck_nodes['locations'] = 
+			$this->finder->query('//td[@class="' . $this->class_location . '"]/text()');	
+	}
+	
+	
+	public function schedule ($options = array()) {
+		$available_options = array(
+			'trucks' => isset($options['trucks']),
+			'days_of_week' => isset($options['days_of_week']),
+			'times_of_day' => isset($options['times_of_day']),
+			'locations' => isset($options['locations'])
+		);
+		$has_days = isset($options['days_of_week']);
+		$has_times = isset($options['times_of_day']);
+		$has_locations = isset($options['locations']);
+		
+		// Copy set options into their own array
+		$set_options = array();
+		foreach ($available_options as $key => $value) {
+			if ($value) {
+				$set_options[$key] = $value;
+			}
+		}
+		
+		// Go through all food trucks
+		for ($i = 0; $i < $this->food_truck_nodes['locations']->length; $i++) {
+			$matches = 0;
+			
+			// Perform filters on this food truck
+			foreach ($set_options as $key => $value) {
+				if ($value and call_user_func_array(array($this, $key), array($i, $options[$key]))) {
+					$matches++;
+				}
+			}
+			
+			// If all filters pass, add this truck to the list
+			if ($matches == sizeof($set_options)) {
+				$this->_add_food_truck($i);
+			}
+		}
+		
+		
+		return $this->food_truck_output;
+	}
+	
+	
+	// Filtering functions
+	private function filter ($list_name, $list_index, $array) {
+		return in_array($this->food_truck_nodes[$list_name]->item($list_index)->nodeValue, $array);
+	}
+	
+	
+	private function trucks ($list_index, $array) {
+		return $this->filter('trucks', $list_index, $array);
+	}
+	
+	
+	private function days_of_week ($list_index, $array) {
+		return $this->filter('days_of_week', $list_index, $array);
+	}
+	
+	
+	private function times_of_day ($list_index, $array) {
+		return $this->filter('times_of_day', $list_index, $array);
+	}
+	
+	
+	private function locations ($list_index, $array) {
+		return $this->filter('locations', $list_index, $array);
+	}
+	
+	
+	// Add food truck to the final list
+	private function _add_food_truck ($list_index) {
+		$this->food_truck_output['food_trucks'][] = array(
+			'company' => $this->food_truck_nodes['companies']->item($list_index)->nodeValue,
+			'company_url' => $this->food_truck_nodes['company_urls']->item($list_index)->nodeValue,
+			'day_of_week' => $this->food_truck_nodes['days_of_week']->item($list_index)->nodeValue,
+			'time_of_day' => $this->food_truck_nodes['times_of_day']->item($list_index)->nodeValue,
+			'location' => $this->food_truck_nodes['locations']->item($list_index)->nodeValue
+		);
+	}
+	
+	
+	// Show lunch trucks today
+	public function trucks_now () {
+		return $this->schedule(array(
+			'days_of_week' => array(date('l')),
+			'times_of_day' => array('Lunch')
+		));
+	}
+}
---
+++ js/app.js
@@ -0,0 +1,52 @@
+
+//Global CONSTANTS
+var dayOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var timeOfDay = ["Breadfast", "Lunch", "Dinner"];
+
+(function() {
+	var app = angular.module('foodTruck',[]);
+
+	app.controller('foodTruckController', ['$http', '$scope', function($http, $scope) {
+		//INITIALIZE
+		$scope.trucks = [];
+
+		$http.get('/food-truck-api/http.php').success(function(data) {
+			var allTrucks = data.food_trucks;
+			// allTrucks.forEach(function(truck, i) {
+			// 	truck.id = i;
+			// });
+			//console.log(allTrucks);
+			$scope.trucks = allTrucks;
+			$scope.setCurrentFilter();
+		});
+
+		$scope.dayOfWeekOrder = function(item) {
+   			return dayOfWeek.indexOf(item.day_of_week);
+		};
+
+		$scope.timeOfDayOrder = function(item) {
+   			return timeOfDay.indexOf(item.time_of_day);
+		};
+
+		//get current day and time and set up filters
+		$scope.setCurrentFilter = function() {
+			var currentdate = new Date();
+			var currentDay = currentdate.getDay();
+			var currentHour = currentdate.getHours();
+			var currentFilter = {};
+			currentFilter.day_of_week = dayOfWeek[currentDay-1];
+			if (currentHour <= 15) {
+				currentFilter.time_of_day = timeOfDay[1];
+			} else {
+				currentFilter.time_of_day = timeOfDay[2];
+			}
+			$scope.selectTruck = currentFilter;
+		}
+	}]);
+
+	app.filter('unique', function() {
+		return function (arr, field) {
+			return _.uniq(arr, function(a) { return a[field]; });
+		};
+	});
+})();
